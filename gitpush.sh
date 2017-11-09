4177bfafba85a6cb1d6e4092424b6520fdfb5a53
liyongan <hzliyongan@corp.netease.com>
2017-11-09 12:31:29 +0800

    创建条目

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,9 @@
 git add .
-git pull origin master
 git commit -m "创建条目"
+git pull origin master
 git push origin master
 cd output/
 git add .
-git pull origin master
 git commit -m "创建条目"
+git pull origin master
 git push origin master
