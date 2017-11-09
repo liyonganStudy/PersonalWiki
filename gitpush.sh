9f40a8cca5366f3950433055edf7ef4dd4ecb8fe
李永安 <hzliyongan@corp.netease.com>
2017-11-09 12:27:19 +0800

    创建条目

--- gitpush.sh
+++ gitpush.sh
@@ -1,8 +1,9 @@
 git add .
+git pull origin master
 git commit -m "创建条目"
 git push origin master
 cd output/
-git pull origin master
 git add .
+git pull origin master
 git commit -m "创建条目"
 git push origin master
